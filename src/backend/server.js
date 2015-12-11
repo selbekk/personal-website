bf2c9d9a92d0dc537578ec781aa1fad910e14db6
selbekk <kristofer.selbekk@bekk.no>
2015-12-10 16:08:41 -0800

    Create a very simple blog based on markdown

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const fs = require('fs');
 const compression = require('compression');
+const marked = require('marked');
 const app = express();
 
 app.engine('handlebars', exphbs({
@@ -18,6 +20,17 @@ app.use('/assets', express.static('dist'));
 
 // Routes
 app.get('/', (req, res) => res.render('index') );
+app.get('/blog/:blogId', (req, res, next) => {
+    const CONTENT_DIR = __dirname + '/../content';
+    const slug = req.params.blogId + '.md';
+    fs.readFile(CONTENT_DIR + '/' + slug, 'UTF-8', (err, content) => {
+        if(err) {
+            return next();
+        }
+        res.render('blog-post', { body: marked(content) });
+    });
+});
+
 
 // Error handling
 app.use((req, res, next) => {
